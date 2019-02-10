8e9f8a500067746951462ab5afd3ca275671a035
beepscore <support@beepscore.com>
2019-02-10 13:15:30 -0800

    add_job with a pre instantiated trigger didn't work, I didn't figure out correct syntax. Instead call add_job, implicitly create the trigger

--- scheduler.py
+++ scheduler.py
@@ -5,7 +5,6 @@
 # flask-apscheduler adds support for flask context but I think that's not needed here
 # https://apscheduler.readthedocs.io/en/latest/userguide.html
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
 
 import datetime
 
@@ -36,31 +35,35 @@ def add_ir_jobs(scheduler):
     QuietTime = namedtuple('QuietTime', 'start end')
 
     # datetime.time: An idealized time, independent of any particular day
-    # TODO: consider implement weekdays e.g. via datetime.datetime
     # TODO: consider read quiet_times from a json file
     quiet_times = [
-        QuietTime(datetime.time(hour=12, minute=1, second=0), datetime.time(hour=12, minute=2, second=0)),
-        QuietTime(datetime.time(hour=12, minute=3, second=0), datetime.time(hour=12, minute=4, second=0)),
-        QuietTime(datetime.time(hour=12, minute=5, second=0), datetime.time(hour=12, minute=6, second=0)),
-        QuietTime(datetime.time(hour=12, minute=7, second=0), datetime.time(hour=12, minute=8, second=0)),
-        QuietTime(datetime.time(hour=12, minute=9, second=0), datetime.time(hour=12, minute=10, second=0)),
+        QuietTime(datetime.time(hour=13, minute=1, second=0), datetime.time(hour=13, minute=2, second=0)),
+        QuietTime(datetime.time(hour=13, minute=3, second=0), datetime.time(hour=13, minute=4, second=0)),
+        QuietTime(datetime.time(hour=13, minute=5, second=0), datetime.time(hour=13, minute=6, second=0)),
+        QuietTime(datetime.time(hour=13, minute=7, second=0), datetime.time(hour=13, minute=8, second=0)),
+        QuietTime(datetime.time(hour=13, minute=9, second=0), datetime.time(hour=13, minute=10, second=0))
     ]
 
     for quiet_time in quiet_times:
 
-        # explicitly instantiating CronTrigger is a little more verbose but hopefully more clear.
-        # Alternatively could write as
-        # scheduler.add_job(transmit_command_ir(RemoteCommand.VOLUME_DECREASE), cron,
-        #                  hour=quiet_time.start.hour, minute=quiet_time.start.minute, second=quiet_time.start.second)
-        trigger_start = CronTrigger(hour=quiet_time.start.hour,
-                                    minute=quiet_time.start.minute,
-                                    second=quiet_time.start.second)
-        trigger_end = CronTrigger(hour=quiet_time.end.hour,
-                                  minute=quiet_time.end.minute,
-                                  second=quiet_time.end.second)
+        # explicitly instantiating CronTrigger is more verbose but I hoped it would be more clear
+        # trigger_start = CronTrigger(hour=quiet_time.start.hour,
+        #                             minute=quiet_time.start.minute,
+        #                             second=quiet_time.start.second)
+        # add_job with a pre instantiated trigger didn't work, I didn't figure out correct syntax
+        # scheduler.add_job(transmit_command_ir, cron, trigger_start, args=[RemoteCommand.VOLUME_DECREASE])
+        # scheduler.add_job(transmit_command_ir_volume_decrease, cron, trigger_start)
 
-        scheduler.add_job(transmit_command_ir(RemoteCommand.VOLUME_DECREASE), cron, trigger_start)
-        scheduler.add_job(transmit_command_ir(RemoteCommand.VOLUME_INCREASE), cron, trigger_end)
+        # add_job, implicitly create the trigger
+        # args is for function transmit_command_ir
+        scheduler.add_job(transmit_command_ir, cron,
+                          day_of_week='mon-sat',
+                          hour=quiet_time.start.hour, minute=quiet_time.start.minute, second=quiet_time.start.second,
+                          args=[RemoteCommand.VOLUME_DECREASE])
+        scheduler.add_job(transmit_command_ir, cron,
+                          day_of_week='mon-sat',
+                          hour=quiet_time.end.hour, minute=quiet_time.end.minute, second=quiet_time.end.second,
+                          args=[RemoteCommand.VOLUME_INCREASE])
 
 
 if __name__ == '__main__':
