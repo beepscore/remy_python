768c59f2676048a16d143160f56e46c06f27dd69
beepscore <support@beepscore.com>
2019-02-10 17:06:35 -0800

    Extract functions add_jobs_volume, add_jobs_mute. In quiet_times_debug add more times.

--- scheduler.py
+++ scheduler.py
@@ -12,6 +12,41 @@ from collections import namedtuple
 from remote_command import RemoteCommand
 from ir_remote import transmit_command_ir
 
+cron = 'cron'
+
+QuietTime = namedtuple('QuietTime', 'start end')
+
+# datetime.time: An idealized time, independent of any particular day
+# TODO: consider read quiet_times from a json file
+quiet_times_debug = [
+    QuietTime(datetime.time(hour=17, minute=1, second=0), datetime.time(hour=17, minute=2, second=0)),
+    QuietTime(datetime.time(hour=17, minute=3, second=0), datetime.time(hour=17, minute=4, second=0)),
+    QuietTime(datetime.time(hour=17, minute=5, second=0), datetime.time(hour=17, minute=6, second=0)),
+    QuietTime(datetime.time(hour=17, minute=7, second=0), datetime.time(hour=17, minute=8, second=0)),
+    QuietTime(datetime.time(hour=17, minute=9, second=0), datetime.time(hour=17, minute=10, second=0)),
+    QuietTime(datetime.time(hour=17, minute=11, second=0), datetime.time(hour=17, minute=12, second=0)),
+    QuietTime(datetime.time(hour=17, minute=13, second=0), datetime.time(hour=17, minute=14, second=0)),
+    QuietTime(datetime.time(hour=17, minute=15, second=0), datetime.time(hour=17, minute=16, second=0)),
+    QuietTime(datetime.time(hour=17, minute=17, second=0), datetime.time(hour=17, minute=18, second=0)),
+    QuietTime(datetime.time(hour=17, minute=19, second=0), datetime.time(hour=17, minute=20, second=0))
+    QuietTime(datetime.time(hour=17, minute=21, second=0), datetime.time(hour=17, minute=22, second=0)),
+    QuietTime(datetime.time(hour=17, minute=23, second=0), datetime.time(hour=17, minute=24, second=0)),
+    QuietTime(datetime.time(hour=17, minute=25, second=0), datetime.time(hour=17, minute=26, second=0)),
+    QuietTime(datetime.time(hour=17, minute=27, second=0), datetime.time(hour=17, minute=28, second=0)),
+    QuietTime(datetime.time(hour=17, minute=29, second=0), datetime.time(hour=17, minute=30, second=0)),
+    QuietTime(datetime.time(hour=17, minute=31, second=0), datetime.time(hour=17, minute=32, second=0)),
+    QuietTime(datetime.time(hour=17, minute=33, second=0), datetime.time(hour=17, minute=34, second=0)),
+    QuietTime(datetime.time(hour=17, minute=35, second=0), datetime.time(hour=17, minute=36, second=0)),
+    QuietTime(datetime.time(hour=17, minute=37, second=0), datetime.time(hour=17, minute=38, second=0))
+]
+
+quiet_times18 = [
+    QuietTime(datetime.time(hour=18, minute=1, second=0), datetime.time(hour=18, minute=2, second=0)),
+    QuietTime(datetime.time(hour=18, minute=3, second=0), datetime.time(hour=18, minute=4, second=0)),
+    QuietTime(datetime.time(hour=18, minute=5, second=0), datetime.time(hour=18, minute=6, second=0)),
+    QuietTime(datetime.time(hour=18, minute=7, second=0), datetime.time(hour=18, minute=8, second=0)),
+    QuietTime(datetime.time(hour=18, minute=9, second=0), datetime.time(hour=18, minute=10, second=0))
+]
 
 def schedule_jobs():
     """ Calls remote control functions based on time of day
@@ -29,34 +64,11 @@ def add_ir_jobs(scheduler):
     https://apscheduler.readthedocs.io/en/latest/modules/triggers/cron.html#module-apscheduler.triggers.cron
     :param scheduler: an APScheduler
     """
+    # add_jobs_volume(quiet_times18, scheduler)
+    add_jobs_mute(quiet_times_debug, scheduler)
 
-    cron = 'cron'
 
-    QuietTime = namedtuple('QuietTime', 'start end')
-
-    # datetime.time: An idealized time, independent of any particular day
-    # TODO: consider read quiet_times from a json file
-    quiet_times_debug = [
-        QuietTime(datetime.time(hour=16, minute=21, second=0), datetime.time(hour=16, minute=22, second=0)),
-        QuietTime(datetime.time(hour=16, minute=23, second=0), datetime.time(hour=16, minute=24, second=0)),
-        QuietTime(datetime.time(hour=16, minute=25, second=0), datetime.time(hour=16, minute=26, second=0)),
-        QuietTime(datetime.time(hour=16, minute=27, second=0), datetime.time(hour=16, minute=28, second=0)),
-        QuietTime(datetime.time(hour=16, minute=29, second=0), datetime.time(hour=16, minute=30, second=0)),
-        QuietTime(datetime.time(hour=16, minute=31, second=0), datetime.time(hour=16, minute=32, second=0)),
-        QuietTime(datetime.time(hour=16, minute=33, second=0), datetime.time(hour=16, minute=34, second=0)),
-        QuietTime(datetime.time(hour=16, minute=35, second=0), datetime.time(hour=16, minute=36, second=0)),
-        QuietTime(datetime.time(hour=16, minute=37, second=0), datetime.time(hour=16, minute=38, second=0))
-    ]
-
-    quiet_times18 = [
-        QuietTime(datetime.time(hour=18, minute=1, second=0), datetime.time(hour=18, minute=2, second=0)),
-        QuietTime(datetime.time(hour=18, minute=3, second=0), datetime.time(hour=18, minute=4, second=0)),
-        QuietTime(datetime.time(hour=18, minute=5, second=0), datetime.time(hour=18, minute=6, second=0)),
-        QuietTime(datetime.time(hour=18, minute=7, second=0), datetime.time(hour=18, minute=8, second=0)),
-        QuietTime(datetime.time(hour=18, minute=9, second=0), datetime.time(hour=18, minute=10, second=0))
-    ]
-
-    quiet_times = quiet_times18 + quiet_times_debug
+def add_jobs_volume(quiet_times, scheduler):
 
     for quiet_time in quiet_times:
 
@@ -78,6 +90,21 @@ def add_ir_jobs(scheduler):
                           day_of_week='mon-sat',
                           hour=quiet_time.end.hour, minute=quiet_time.end.minute, second=quiet_time.end.second,
                           args=[RemoteCommand.VOLUME_INCREASE])
+
+
+def add_jobs_mute(quiet_times, scheduler):
+
+    for quiet_time in quiet_times:
+        # add_job, implicitly create the trigger
+        # args is for function transmit_command_ir
+        scheduler.add_job(transmit_command_ir, cron,
+                          day_of_week='mon-sat',
+                          hour=quiet_time.start.hour, minute=quiet_time.start.minute, second=quiet_time.start.second,
+                          args=[RemoteCommand.MUTE])
+        scheduler.add_job(transmit_command_ir, cron,
+                          day_of_week='mon-sat',
+                          hour=quiet_time.end.hour, minute=quiet_time.end.minute, second=quiet_time.end.second,
+                          args=[RemoteCommand.MUTE])
 
 
 if __name__ == '__main__':
